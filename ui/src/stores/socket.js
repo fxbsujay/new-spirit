6f1ea243462e438bb12f0ca8d25c646d29522a01
sujay@fengkeai.com <sujay@fengkeai.com>
2025-07-31 10:23:17 +0800

    socket

---
+++ ui/src/stores/socket.js
@@ -0,0 +1,24 @@
+import { defineStore } from 'pinia'
+
+export const useSocketStore = defineStore('counter', () => {
+
+  const socket = new WebSocket(`${window.location.protocol === 'https:' ? 'wss:' : 'ws:'}//${window.location.host}/api/ws`)
+
+  socket.onopen = function (event) {
+    console.log('WebSocket 连接成功', event);
+  }
+  
+  socket.onclose = function(event) {
+    if (event.wasClean) {
+      console.log(`连接已正常关闭，代码=${event.code}，原因=${event.reason}`);
+    } else {
+      console.log('连接中断');
+    }
+  }
+
+  const isConnected = () => {
+    return socket.readyState === WebSocket.OPEN
+  }
+  return { socket, isConnected }
+})
+
