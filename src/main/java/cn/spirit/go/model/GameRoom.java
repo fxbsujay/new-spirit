9ce7433d3a3b86e20ed9f6ece54ee4e58c405fa5
sujay <sujay@fengkeai.com>
2025-12-17 18:21:21 +0800

    4234

--- src/main/java/cn/spirit/go/model/GameRoom.java
+++ src/main/java/cn/spirit/go/model/GameRoom.java
@@ -3,9 +3,7 @@ package cn.spirit.go.model;
 import cn.spirit.go.common.enums.GameMode;
 import cn.spirit.go.common.enums.GameType;
 
-import java.util.HashSet;
-import java.util.Set;
-import java.util.Objects;
+import java.util.*;
 
 public class GameRoom {
 
@@ -17,7 +15,7 @@ public class GameRoom {
     /**
      * 步骤
      */
-    public Set<GameStep> steps = new HashSet<>();
+    public List<GameStep> steps = new ArrayList<>();
 
     /**
      * 客户端链接
@@ -25,30 +23,47 @@ public class GameRoom {
     public Set<GameSocket> sockets = new HashSet<>();
 
     /**
-     * 白-剩余时间 毫秒 -1 为未开始计时或无需计时
+     * 白-每一步剩余时间的累计 为
      */
-    public Long whiteTime = -1L;
+    public Long whiteRemainingTime = 0L;
 
     /**
-     * 黑-剩余时间 毫秒 -1 为未开始计时或无需计时
+     * 黑-每一步剩余时间的累计
      */
-    public Long blackTime = -1L;
+    public Long blackRemainingTime = 0L;
 
     /**
-     * 用户操作超时了
-     * @param username  玩家用户名
+     * 用户这一步操作的剩余时间
+     * @param timestamp  操作时间戳
+     * @return 超时多长时间 小于0 为为超时
      */
-    public boolean isTimeOut(String username) {
+    public long remainingTime(long timestamp) {
         if (GameType.NONE == info.type) {
-            // 对局无时间限制或对局处在开始阶段
+            // 对局无时间限制
+            throw new RuntimeException("Game type is NONE, Unable to calculate remaining duration");
+        }
+        int size = steps.size();
+        if (size <= 2)  {
+            // 对局前两手不计算时长
+            throw new RuntimeException("The duration of the first two steps of a game match is not counted");
+        }
+
+        // 剩余时间 = 设定的每步加时时长 - (当前时间 - 开始计时时间))
+        return info.stepDuration * 1000 - (timestamp - steps.get(steps.size() - 1).timestamp);
+    }
+
+    /**
+     * 步骤操作是否超时
+     */
+    public boolean isTimeout(long timestamp) {
+        if (info.type == GameType.NONE || steps.size() <= 2) {
             return false;
         }
+        return (remainingTime(timestamp) + (steps.size() % 2 == 0 ? blackRemainingTime : whiteRemainingTime) + info.duration * 1000) <= 0;
+    }
 
-        if (info.white.equals(username)) {
-
-        }
-
-        return false;
+    public boolean isTimeout() {
+        return isTimeout(System.currentTimeMillis());
     }
 
     /**
@@ -56,10 +71,37 @@ public class GameRoom {
      * @param step  步骤
      */
     public boolean addStep(GameStep step) {
-        if (GameType.NONE != info.type) {
-
+        if (steps.contains(step)) {
+            return false;
         }
+        if (info.type != GameType.NONE) {
+            int size = steps.size();
+            if (size > 2) {
+                long remainingTime = remainingTime(step.timestamp) + (size % 2 == 0 ? blackRemainingTime : whiteRemainingTime);
+                if (size % 2 == 0) {
+                    blackRemainingTime += remainingTime;
+                    if (blackRemainingTime + info.duration * 1000 < 0) {
+                        // TODO 黑方超时 白方胜
+                        return false;
+                    }
+                } else {
+                    whiteRemainingTime += remainingTime;
+                    if (whiteRemainingTime + info.duration * 1000 < 0) {
+                        // TODO 白方超时 黑方胜
+                        return false;
+                    }
+                }
+            }
+        }
+
         return steps.add(step);
+    }
+
+    /**
+     * 游戏结束 操作超时, 认输, 棋盘已满
+     */
+    public void finish() {
+
     }
 
     @Override
